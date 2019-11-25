a8b486c623604ebcc42fc3c3cc6434111b824c4e
SongLL <lai540265376@qq.com>
2019-11-25 14:52:14 +0800

    参数

---
+++ key words.py
@@ -0,0 +1,17 @@
+#def message(name,age,*,city='fujian',juice):
+#    print(name,age,city,juice)
+
+#def message(name,age,**kw):
+#    if 'city' in kw:
+#        pass
+#    if 'juice' in kw:
+#        pass
+#    print(name,age,kw)
+#调用者在限制条件下仍然可以传入
+
+#def product(x,*z):
+#    sum = x
+#    for n in z:
+#        sum = sum * n
+#    return sum
+#计算多个数的乘积
